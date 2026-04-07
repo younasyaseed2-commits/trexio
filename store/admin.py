b8ee41ad68c9319264ba90cbb6fb07c108ba9b97
Younas Yaseed <younasyaseed2@gmail.com>
2026-04-07 08:36:29 +0530

    Add custom domain to ALLOWED_HOSTS

--- store/admin.py
+++ store/admin.py
@@ -1,28 +1,38 @@
 from django.contrib import admin
-from .models import User, Category, Product, ProductGallery, Cart, CartItem, Address
+from .models import (
+    User, Category, Product, ProductGallery,
+    Cart, CartItem, Address, Order, OrderItem
+)
 
 # 1. Setup the Gallery to show up inside the Product page
 class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
-    extra = 3 # Shows 3 empty upload boxes by default
+    extra = 1
 
-# 2. Attach the Gallery to the Product Admin
+# 2. Custom Product Admin (Removed the problematic 'slug' and 'list_display' fields)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'is_available')
-    inlines = [ProductGalleryInline] # This injects the gallery!
+    inlines = [ProductGalleryInline]
+    # We only show 'name' and 'price' to be safe, as these are standard
+    list_display = ['name', 'price']
 
-# Register your models
+# 3. Setup Order Items to show up inside the Order page
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+# --- Register your models ---
+
 admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(Product, ProductAdmin) # Use the custom admin
+
+# IMPORTANT: We register Product with our custom ProductAdmin class
+admin.site.register(Product, ProductAdmin)
+
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Address)
-# Change your import line at the top to look like this:
-from .models import User, Category, Product, ProductGallery, Cart, CartItem, Address, Order, OrderItem
-
-# ... (Keep your existing admin code here) ...
-
-# Add these two lines at the very bottom:
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
