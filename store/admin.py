44382ff72d1d4d2abb7dc53602fda45330333a24
Younas Yaseed <younasyaseed2@gmail.com>
2026-04-26 18:52:45 +0530

    Fix duplicate OrderTracking registration causing deploy crash

--- store/admin.py
+++ store/admin.py
@@ -1,58 +1,44 @@
 from django.contrib import admin
 from .models import (
     User, Category, Product, ProductGallery,
-    Cart, CartItem, Address, Order, OrderItem
+    Cart, CartItem, Address, Order, OrderItem, OrderTracking
 )
-from .models import OrderTracking
-from django.contrib import admin
-from .models import Order, OrderItem, OrderTracking
+
+# 1. INLINES (Used inside other pages)
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
 
+# 2. CUSTOM ADMIN CLASSES
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'is_available']
+    list_editable = ['price', 'stock', 'is_available']
+    inlines = [ProductGalleryInline]
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    # This makes the admin look professional
     list_display = ['id', 'user', 'total_price', 'status', 'created_at']
     list_filter = ['status', 'created_at']
     search_fields = ['id', 'user__username', 'address__full_name']
-    list_editable = ['status'] # Change status directly from the list!
+    list_editable = ['status']
     inlines = [OrderItemInline]
 
 @admin.register(OrderTracking)
 class OrderTrackingAdmin(admin.ModelAdmin):
     list_display = ['order', 'location', 'created_at']
+    list_filter = ['created_at']
 
-# 1. Setup the Gallery to show up inside the Product page
-class ProductGalleryInline(admin.TabularInline):
-    model = ProductGallery
-    extra = 1
-
-# 2. Custom Product Admin (Removed the problematic 'slug' and 'list_display' fields)
-class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductGalleryInline]
-    # We only show 'name' and 'price' to be safe, as these are standard
-    list_display = ['name', 'price']
-
-# 3. Setup Order Items to show up inside the Order page
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
-
-class OrderAdmin(admin.ModelAdmin):
-    inlines = [OrderItemInline]
-
-# --- Register your models ---
-
+# 3. SIMPLE REGISTRATIONS
+# Note: Do NOT manually register Order, Product, or OrderTracking here
+# because we used @admin.register above.
 admin.site.register(User)
 admin.site.register(Category)
-
-# IMPORTANT: We register Product with our custom ProductAdmin class
-admin.site.register(Product, ProductAdmin)
-
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Address)
-admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
